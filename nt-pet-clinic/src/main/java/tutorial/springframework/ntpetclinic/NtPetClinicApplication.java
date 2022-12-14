81332f9e3f2b59237e76cdfaa61e66c737896ab5
FNBJNB01\F5336775 <Ntethelelo.Zikalala@fnb.co.za>
2022-12-14 18:12:45 +0200

    Creating Projrct from Initializer

---
+++ nt-pet-clinic/src/main/java/tutorial/springframework/ntpetclinic/NtPetClinicApplication.java
@@ -0,0 +1,13 @@
+package tutorial.springframework.ntpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtPetClinicApplication.class, args);
+	}
+
+}
---
+++ nt-pet-clinic/src/test/java/tutorial/springframework/ntpetclinic/NtPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package tutorial.springframework.ntpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
